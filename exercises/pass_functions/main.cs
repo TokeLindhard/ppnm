80d2b23b6a36b1eff108cea951d4b143c7ac62a3
Toke Lindhard <tokelindhard@gmail.com>
2022-02-20 20:53:05 +0100

    added passf exercise

---
+++ exercises/pass_functions/main.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+    public static void Main(){
+        Func<double,double,double> f = delegate(double x, double k){
+            return Sin(k*x);
+        };
+    for (int i=1; i<=3; i++){
+        table.make_table(x => f(x,i),1,5,1);
+        WriteLine("\n");
+    }
+    }
+}
