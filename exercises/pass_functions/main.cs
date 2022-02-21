39a06ca7ecc62b7c35faaa54328bb7e5a0fb8d88
Toke Lindhard <tokelindhard@gmail.com>
2022-02-21 17:50:53 +0100

    fix changes

--- exercises/pass_functions/main.cs
+++ exercises/pass_functions/main.cs
@@ -7,9 +7,9 @@ class main{
         Func<double,double,double> f = delegate(double x, double k){
             return Sin(k*x);
         };
-    for (int i=1; i<=3; i++){
-        table.make_table(x => f(x,i),1,5,1);
-        WriteLine("\n");
-    }
+        for (int i=1; i<=3; i++){
+            table.make_table(x => f(x,i),1,5,1);
+            WriteLine("\n");
+        }
     }
 }
