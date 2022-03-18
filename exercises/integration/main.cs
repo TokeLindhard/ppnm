363c6bf557796a2508c2be377ecc0e9ed1042b61
Toke Lindhard <tokelindhard@gmail.com>
2022-03-18 15:36:15 +0100

    finished odeint exercise

--- exercises/integration/main.cs
+++ exercises/integration/main.cs
@@ -11,10 +11,16 @@ class main{
     public static void Main(){
         Func<double,double> f = delegate(double x){return Log(x)/Sqrt(x);};
         double result1 = integrate.quad(f,0,1);
-        WriteLine($"result1 is {result1}");
+        WriteLine($"#result1 is {result1}"); //making the # comment so the pyxplot will ignore it.
+        //otherwise I would have to make two seperate files, which I am too lazy to do.
 
         //result for error function
         double result2 = erf(2);
-        WriteLine($"result2 is {result2}");
+        WriteLine($"#result2 is {result2}");
+
+        //For plotting the error function
+        for(double x=0;x<=2;x+=1.0/8){
+            WriteLine($"{x} {erf(x)}");
+        }
     }
 }
