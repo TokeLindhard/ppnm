c0f6a6df4c822f3997bf5b8a11601e09551eec60
Toke Lindhard <tokelindhard@gmail.com>
2022-05-15 13:59:20 +0200

    Splines part A

--- homeworks/Splines/A/interpolation.cs
+++ homeworks/Splines/A/interpolation.cs
@@ -4,18 +4,34 @@ using static System.Math;
 using System.Diagnostics;
 
 public static class interpol{
-	static double linterp(double[]x, double[]y, double z){
-        Debug.Assert(n>1 && z>=x[0] && z<=x[n-1]); //makes sure our n and z aren't running rampant
-        double dy=y[i+1]-y[i], dx=x[i+1]-x[i]; Debug.Assert(dx>0); //makes sure stepsize is actually positive
-        return y[i]+dy/dx*(z-x[i]);
+	public static double linterp(double[]x, double[]y, double z){
+			int i=binsearch(x,z);
+        	double dx=x[i+1]-x[i]; if(!(dx>0)) throw new Exception("uups...");
+        	double dy=y[i+1]-y[i];
+        	return y[i]+dy/dx*(z-x[i]);
     }
-public static int binsearch(double[] x, double z){// locates the interval for z by bisection 
-	int i=0, j=x.Length-1;
-	while(j-i>1){
-		int mid=(i+j)/2;
-		if(z>x[mid]) i=mid; 
-		else j=mid;
+	public static int binsearch(double[] x, double z){// locates the interval for z by bisection 
+		int i=0, j=x.Length-1;
+		while(j-i>1){
+			int mid=(i+j)/2;
+			if(z>x[mid]) i=mid; 
+			else j=mid;
 		}
-	return i;
+		return i;
+	}
+
+	public static double linterpInteg(double[] x, double[] y, double z){
+		double integral = 0;
+		int idx = binsearch(x,z);
+		for(int i=0; i<idx;i++){
+			double dx = x[i+1]-x[i];	
+			double dy = y[i+1]-y[i];
+			double pi = dy/dx; //eq 6 from notes
+			integral += y[i]*(x[i+1]-x[i])+pi*Pow(x[i+1]-x[i],2)/2; //eq. 8
+		}
+		double p = (y[idx+1]-y[idx])/(x[idx+1]-x[idx]);
+		integral += y[idx]*(z-x[idx])+p*Pow(z-x[idx],2)/2;
+		return integral;
 	}
 }
+
--- homeworks/Splines/A/main.cs
+++ homeworks/Splines/A/main.cs
@@ -5,9 +5,27 @@ using System.Diagnostics;
 
 class main{
     public static void Main(){
-        double[] a = new double[] {1,2,3,4,5,6,7}; //2nd degree polynomial
-        double[] b = new double[] {1,4,9,16,25,36,49};
-        interpolation.interpol linterp(10,a,b,5);
+        double[] xs = new double[] {1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0,7.5,8.0,8.5,9.0,9.5,10.0}; //2nd degree polynomial
+        double[] ys = new double[xs.Length];
+        for(int i =0; i<ys.Length; i++){ //Making 2nd degree polynomial from the x-values
+            //ys[i] = Sin(xs[i]);
+            ys[i] = Pow(xs[i],2);
+        }
+        //interpolation.interpol linterp(xs,ys,15);
+
+        using(var outfile = new System.IO.StreamWriter("2ndpoly.txt")){
+            for(int i = 0; i<ys.Length;i++){
+                outfile.WriteLine($"{xs[i]} {ys[i]}");
+            }
+        }
+
+        using(var outfile = new System.IO.StreamWriter("integration.txt")){
+            for(double z=0; z<10;z+=1.0/64){
+                outfile.WriteLine($"{z} {interpol.linterp(xs,ys,z)} {interpol.linterpInteg(xs,ys,z)}");
+            }
+        }
+
+
         //interpolationinterpol.linterp(10,x,y,5);
         //WriteLine(string.Join("\n", x));
         //WriteLine(string.Join("\n",y));
