8b6417e1b9d35895008191320c7f07fbf96eec79
Toke Lindhard <tokelindhard@gmail.com>
2022-06-23 14:13:15 +0200

    Forgot to do the hard integral. Done now.

--- homeworks/MonteCarlo/A/main.cs
+++ homeworks/MonteCarlo/A/main.cs
@@ -11,7 +11,7 @@ class main{
         Func<vector,double> f1 = v => {return v[0]*v[1];}; //x*y. v is the vector v=(x,y)^T
         vector a1 = new double[] {5,7}; //lower limits for integrals (first is x-integral lower limit, 2nd is for y)
         vector b1 = new double[] {10,12}; //upper limits for integrals (first is x-integral upper limit, 2nd is for y)
-        var intf1 = mc.plainmc(f1,a1,b1,N); //first item it returns is the volume
+        var intf1 = mc.plainmc(f1,a1,b1,N); //first item it returns is the value of the integral, 2nd item is the error
         Write("First, the integral of x*y with lower limits [5,7] and upper limits [10,12]");
         WriteLine($"The integral yields {intf1.Item1} with error {intf1.Item2}");
         WriteLine($"The analytical result is 7125/4=1781.25");
@@ -32,7 +32,17 @@ class main{
         var intf3 = mc.plainmc(f3,a3,b3,N);
         WriteLine("Lastly, we test the integral of x^2+y^2 with lower limits [5,3] and upper limits [10,7]");
         WriteLine($"The integral yields {intf3.Item1} with error {intf3.Item2}");
-        WriteLine("The analytical result is -2");        
+        WriteLine("The analytical result is -2"); 
+        WriteLine();
+        WriteLine();
 
+        WriteLine("Now we are ready to test the 3D-integral of 1/pi^3*int dx*int dy*int dz [1-cos(x)cos(y)cos(z)]^-1");
+        WriteLine("With limits xlim=ylim=zlim=[0,pi]");
+        Func<vector, double> f4 = v => {return 1/(Pow(PI,3))*1/(1-Cos(v[0])*Cos(v[1])*Cos(v[2]));};
+        vector a4 = new double[] {0,0,0};
+        vector b4 = new double[] {PI,PI,PI};
+        var intf4 = mc.plainmc(f4,a4,b4,N);
+        WriteLine($"The integral yields {intf4.Item1} with error {intf4.Item2}");
+        WriteLine("The analytical result is Gamma(1/4)^4/(4pi)^3=1.3932..."); 
     }
 }
