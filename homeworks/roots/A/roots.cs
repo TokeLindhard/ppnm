d403913f61ce7a9ac4189c0fca6c99f495e93257
Toke Lindhard <tokelindhard@gmail.com>
2022-06-27 22:06:54 +0200

    Added Frederikkes repos

---
+++ homeworks/roots/A/roots.cs
@@ -0,0 +1,20 @@
+using System;
+using static System.Math;
+
+public class rootfinder{
+    
+    public static vector newton(Func<vector,vector>f, vector x0, double eps=1e-2){
+        return x;
+    }
+
+    public static double delta = 0; //we change this later, when going through the xi's
+    public static vector jacobi(Func<vector,vector>f, vector x0){ //finds the partial derivatives using eq. 7
+        for(int i=0;i<x0.size;i++){
+            delta = Abs(x0[i])*2e-26;
+            for(int k=0;i<x0.size;k++){
+
+            }
+        }
+        return J;
+    }
+}
